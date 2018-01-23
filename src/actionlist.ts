dfd4f2623f362682a98235e59ed475a2ca0f2e9c
Nathan Kunicki <me@nathankunicki.com>
2018-01-23 09:29:46 +0000

    Start of undo redo

---
+++ src/actionlist.ts
@@ -0,0 +1,12 @@
+/* class ActionList extends LinkedList {
+
+
+    private _current: ActionNode;
+
+
+    public push (node: LinkedNode): void {
+        // super(node);
+    }
+
+
+} */
---
+++ src/actionnode.ts
@@ -0,0 +1,15 @@
+/* class ActionNode extends LinkedNode {
+
+
+    public undo: () => void;
+    public redo: () => void;
+
+
+    constructor (undo: () => void, redo: () => void) {
+        super();
+        this.undo = undo;
+        this.redo = redo;
+    }
+
+
+} */
